8110f1e2bef52cd3b8586d6a5a4ca7ed505401cf
Erik Johnson <erik@talentriot.com>
2014-03-07 13:40:53 -0700

    wired up up & down btns

--- src/InterfacesWithUI/Form1.cs
+++ src/InterfacesWithUI/Form1.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 using DomainAndServices.Domain;
 using DomainAndServices.Interfaces;
@@ -8,6 +11,7 @@ namespace InterfacesWithUI
 {
     public partial class Form1 : Form
     {
+        private List<IDBDisplayable> _reorderableItems = new List<IDBDisplayable>(); 
         public Form1()
         {
             InitializeComponent();
@@ -28,6 +32,7 @@ namespace InterfacesWithUI
         {
             lstLeft.Items.Clear();
             lstRight.Items.Clear();
+            _reorderableItems.Clear();
 
             var foodService = new FoodDataService();
             var foods = foodService.GetAllFoods();
@@ -36,6 +41,7 @@ namespace InterfacesWithUI
             {
                 lstLeft.Items.Add(food);
             }
+            
         }
 
         private void btnToRight_Click(object sender, EventArgs e)
@@ -49,6 +55,13 @@ namespace InterfacesWithUI
             RemoveFromListBox(lstLeft, selectedFood);
 
             AddToListBox(lstRight, selectedFood);
+            AddToReorderableList(selectedFood);
+        }
+
+        private void AddToReorderableList(IDBDisplayable selectedItem)
+        {
+            selectedItem.Index = lstRight.Items.Count - 1;
+            _reorderableItems.Add(selectedItem);
         }
 
         private void btnToLeft_Click(object sender, EventArgs e)
@@ -62,6 +75,14 @@ namespace InterfacesWithUI
             RemoveFromListBox(lstRight, selectedFood);
 
             AddToListBox(lstLeft, selectedFood);
+            RemoveFromReorderableList(selectedFood);
+        }
+
+        private void RemoveFromReorderableList(IDBDisplayable selectedItem)
+        {
+            var removedIndex = selectedItem.Index;
+            _reorderableItems.Where(item => item.Index > removedIndex).ToList().ForEach(item => item.Index--);
+            _reorderableItems.Remove(selectedItem);
         }
 
         private void RemoveFromListBox(ListBox listBox, IDBDisplayable selectedPerson)
@@ -100,6 +121,7 @@ namespace InterfacesWithUI
         {
             lstLeft.Items.Clear();
             lstRight.Items.Clear();
+            _reorderableItems.Clear();
 
             var peopleService = new PersonDataService();
             var people = peopleService.GetAllPeople();
@@ -116,8 +138,30 @@ namespace InterfacesWithUI
             {
                 return;
             }
-            var selectedPerson = lstRight.SelectedItem as Person;
-            var selectedPersonIndex = lstRight.SelectedIndex;
+            var selectedItemIndex = lstRight.SelectedIndex;
+            ReorderListBox(selectedItemIndex, -1);
+        }
+
+        private void ReorderListBox(int selectedItemIndex, int indexIncValue)
+        {
+            if (ItemCannotBeMoved(selectedItemIndex, indexIncValue))
+            {
+                return;
+            }
+            var selectedItem = _reorderableItems.Find(item => item.Index == selectedItemIndex);
+            var itemToSwapWith = _reorderableItems.Find(item => item.Index == selectedItemIndex + indexIncValue);
+            selectedItem.Index += indexIncValue;
+            itemToSwapWith.Index -= indexIncValue;
+            _reorderableItems = _reorderableItems.OrderBy(item => item.Index).ToList();
+            lstRight.Items.Clear();
+            _reorderableItems.ForEach(item => lstRight.Items.Add(item));
+            lstRight.SelectedIndex = selectedItemIndex + indexIncValue;
+        }
+
+        private bool ItemCannotBeMoved(int selectedItemIndex, int indexIncValue)
+        {
+            var nextIndex = selectedItemIndex + indexIncValue;
+            return nextIndex < 0 || nextIndex > lstRight.Items.Count - 1;
         }
 
         private void btnDown_Click(object sender, EventArgs e)
@@ -126,10 +170,8 @@ namespace InterfacesWithUI
             {
                 return;
             }
-            var selectedPerson = lstRight.SelectedItem as Person;
             var selectedPersonIndex = lstRight.SelectedIndex;
-
-
+            ReorderListBox(selectedPersonIndex, 1);
         }
 
         private void cmbTypeOfItems_SelectedIndexChanged(object sender, EventArgs e)
