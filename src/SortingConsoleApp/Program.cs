48ebee54000363dc5cb3c73d129e7dbd16248281
Fernando Cardenas <fernando@talentriot.com>
2014-03-07 05:43:53 -0700

    Added a new consol app to show implementing a .NET interface

---
+++ src/SortingConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DomainAndServices.Domain;
+using DomainAndServices.Services;
+
+namespace SortingConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var foodService = new FoodDataService();
+            var foods = foodService.GetAllFoods();
+
+            foreach (Food food in foods)
+            {
+                Console.WriteLine("{0}", food.Name);
+            }
+
+            Console.WriteLine("Press any key to end this program");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//http://msdn.microsoft.com/en-us/library/system.icomparable(v=vs.110).aspx
